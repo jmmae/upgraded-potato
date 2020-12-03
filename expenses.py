e67dcc65465fa51ea72c2b3eec14193c6fa76259
judithemae <zhac258@live.rhul.ac.uk>
2020-12-03 22:37:09 +0000

    Added getter and setter methods, made while loop to ask user choice

--- expenses.py
+++ expenses.py
@@ -1,8 +1,50 @@
 class Item: 
-    def __init__(self,description,category,amount):
-        self.name = name
-        self.website = website 
-        self.cuisine = cuisine 
+    def __init__(self, description, category, amount):
+        self.description = description
+        self.category = category 
+        self.amount = amount 
 
-    def get_name(self):
-        return self.name
+    def get_description(self):
+        return self.description
+
+    def get_category(self):
+        return self.category
+
+    def get_amount(self):
+        return self.amount
+
+    def set_description(self, description):
+        self.description = description
+
+    def set_category(self, category):
+        self.category = category
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+
+print("Choose between:")
+print("1. Edit Mode")
+print("2. Analysis Mode")
+print("3. Exit")
+userInp = int(input("Please enter your choice: "))
+
+while True:
+    if userInp == 1:
+        editMode()
+    elif userInp == 2:
+        analysisMode()
+    elif userInp == 3:
+        print("Bye!")
+        break
+
+def editMode():
+    pass
+
+def analysisMode():
+    pass
+
+
+
+
+    
