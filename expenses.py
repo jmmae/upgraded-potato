d4fa0466d02753364440116a1c68e69f50d7f348
plainoldcookies <genericguythere@gmail.com>
2020-12-04 11:10:20 +0000

    Added price variable to the item object. Added functionality to editMode, with two new functions createObject and deleteObject, which create / make objects inaccessible.

--- expenses.py
+++ expenses.py
@@ -1,8 +1,11 @@
+itemSet = set()
+
 class Item: 
-    def __init__(self, description, category, amount):
+    def __init__(self, description, category, amount, price):
         self.description = description
         self.category = category 
         self.amount = amount 
+        self.price = price
 
     def get_description(self):
         return self.description
@@ -12,6 +15,9 @@ class Item:
 
     def get_amount(self):
         return self.amount
+    
+    def get_price(self):
+        return self.price
 
     def set_description(self, description):
         self.description = description
@@ -21,15 +27,57 @@ class Item:
 
     def set_amount(self, amount):
         self.amount = amount
+    
+    def set_price (self, price):
+        self.price
+        
+def createObject(): 
+    newItem = str(input("Enter the name of the product: "))
+    newDescription = str(input("Enter description of product: "))
+    newCategory = str(input("Enter category of product: "))
+    newAmount = int(input("Enter amount of product: "))
+    newPrice = int(input("Enter price of product: "))
+    itemSet.add(newItem)
+    newItem = Item(newDescription,newCategory,newAmount,newPrice)
+    print("The itemSet is now", itemSet)
+    
 
+def deleteObject(delete):
+    itemSet.discard(delete) #making something inaccessible is totally the same thing as deleting it
+    print("The itemSet is now", itemSet)
 
-print("Choose between:")
-print("1. Edit Mode")
-print("2. Analysis Mode")
-print("3. Exit")
-userInp = int(input("Please enter your choice: "))
+def editMode():
+    while True:
+        print("Choose between:")
+        print("1. createObject ")
+        print("2. deleteObject ")
+        print("3. Exit to menu screen")
+        userInp = int(input("Please enter your choice: "))
+        if userInp == 1:
+            createObject()
+        if userInp == 2:
+            toDelete = str(input("Enter the name of the product to be deleted: "))
+            deleteObject(toDelete)
+        elif userInp == 3: 
+            break
+    
+
+def analysisMode():
+    print("Choose between:")
+    print("1. categorySpend ")
+    print("2. averageSpend ")
+    print("3. totalSpend")
+    print("4. Exit to menu screen")
+    userInp = int(input("Please enter your choice: "))
+    pass
+
 
 while True:
+    print("Choose between:")
+    print("1. Edit Mode")
+    print("2. Analysis Mode")
+    print("3. Exit")
+    userInp = int(input("Please enter your choice: "))
     if userInp == 1:
         editMode()
     elif userInp == 2:
@@ -38,11 +86,6 @@ while True:
         print("Bye!")
         break
 
-def editMode():
-    pass
-
-def analysisMode():
-    pass
 
 
 
